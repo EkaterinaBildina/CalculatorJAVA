c61c303a3d1a9198f040577571bfed8127e5af83
EkaterinaBildina <kukresch@gmail.com>
2023-09-30 21:19:00 +0300

    UML-diagramm to do based on SOLID princip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import internalToDo.Calculator;
 
 public class Main {
+    // Разработать одну или несколько программ на выбранном языке программирования,
+    // демонстрирующих применение принципов SOLID.
+    // Сопроводить разработку созданием UML-диаграммы,
+    // которая будет отображать ключевые компоненты системы и их взаимосвязи.
+
     public static void main(String[] args) {
         ViewCalculator view = new ViewCalculator(Calculator::new);
         view.run();
